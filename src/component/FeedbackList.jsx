8054ed38ebd9cdf69b6ab81e5e111cdbe51812f7
A-Alafandi <abdulrazak.alafandi@hotmail.com>
2023-09-21 13:06:29 +0200

    feat: feedback list $ feedbackItem

---
+++ src/component/FeedbackList.jsx
@@ -0,0 +1,28 @@
+import FeedbackItem from './FeedbackItem'
+import PropTypes from 'prop-types'
+
+function FeedbackList({ feedback }) {
+  if (!feedback || feedback.length === 0) {
+    return <p>No Feedback Yet</p>
+  }
+
+  return (
+    <div className="feedback-list">
+      {feedback.map((item) => (
+        <FeedbackItem key={item.id} item={item} />
+      ))}
+    </div>
+  )
+}
+
+FeedbackList.porpTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
+}
+
+export default FeedbackList
